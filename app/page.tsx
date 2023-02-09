e2fee1d580ba35d635999cb59436ff7cf49142a4
kinderhead <kinderhead512@gmail.com>
2023-02-08 21:24:08 -0500

    wow

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,27 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
-import { Bar } from 'react-chartjs-2'
+import { Bar, Line } from 'react-chartjs-2'
 import { useState } from 'react'
+import { Chart, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-    const [data, setData] = useState<number[]>([]);
+    Chart.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Legend
+    );
+    const [data, setData] = useState<number[]>([6, 1, 7, 2, 9, 7, 3, 5, 7, 2, 1, 0]);
 
     return (
         <main>
-            <Bar options={{}} data={{labels: data.map(i => i), datasets: [{label: "numbers", data: data}]}}></Bar>
+            <Line options={{}} data={{labels: data.map(i => i), datasets: [{label: "numbers", data: data, backgroundColor: "#000000", borderColor: "#A0A0A0"}]}}></Line>
         </main>
     )
 }
